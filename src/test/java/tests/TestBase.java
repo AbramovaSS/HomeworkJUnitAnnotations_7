60cdf60d8914a3281e72522a3be2b33551bab868
SSAbramova <ssabramova@inno.tech.ru>
2026-01-08 20:35:48 +0300

    Corrected setDate and setTable

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,13 +6,12 @@ import org.junit.jupiter.api.BeforeAll;
 public class TestBase {
 
     @BeforeAll
-    static void beforeAll() {
+    static void setupSelenideConfig() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-//        Configuration.browser = "chrome";
+        Configuration.browser = "chrome";
         Configuration.timeout = 10000;
-//        Configuration.holdBrowserOpen = true;
     }
 
 }
